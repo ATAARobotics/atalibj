6c336084cd613cbb8970e5a32258b15ae4cf0fb2
Joel Gallant <joelgallant236@gmail.com>
2012-10-21 16:23:06 -0600

    Changed to goodbye

--- src/edu/ata/main/GamePeriods.java
+++ src/edu/ata/main/GamePeriods.java
@@ -28,6 +28,6 @@ public class GamePeriods extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-	Hello
+	Goodbye Cruel World
     }
 }
