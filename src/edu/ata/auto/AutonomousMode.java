f7bec8b3f709acc3829aa4069d5c2e561295c947
joel <joel@joel-laptop>
2012-12-10 22:29:17 -0700

    Merged name capability with Command and CommandGroup. Fixed some documentation.

--- src/edu/ata/auto/AutonomousMode.java
+++ src/edu/ata/auto/AutonomousMode.java
@@ -14,38 +14,34 @@ import edu.ata.commands.CommandGroup;
  * {@code new AutonomousMode(Name)} is wrong.
  *
  * <p> To add commands to the Autonomous mode, use the commands in
- * {@link CommandGroup}. ({@code addSequential()}, {@code addConcurrent()}.
+ * {@link CommandGroup}. ({@link CommandGroup#addSequential(edu.ata.commands.Command) addSequential()},
+ * {@link CommandGroup#addConcurrent(edu.ata.commands.Command) addConcurrent()}).
+ *
+ * <p> AutonomousMode is identical to {@link CommandGroup}. There is no
+ * difference between them. AutonomousMode is here to identify CommandGroups
+ * that are full autonomous modes. It serves as a type-safe way to tell the
+ * program that it is a full autonomous mode.
  *
  * @see CommandGroup
  * @author Joel Gallant
  */
 public class AutonomousMode extends CommandGroup {
 
-    private final String name;
-
     /**
      * Creates the autonomous mode with its name. Should add commands in
      * constructor.
      *
-     * <p> The name of an autonomous mode should be short, concise and to the point.
-     * It should outline its behavior but still say only what is needed. The
-     * only purpose of names are to identify what is running to the user. It is
-     * not used to compare objects.
+     * <p> The name of an autonomous mode should be short, concise and to the
+     * point. It should outline its behavior but still say only what is needed.
+     * The only purpose of names are to identify what is running to the user. It
+     * is not used to compare objects. The user should be able to tell what the
+     * autonomous mode does based on it's name only.
+     *
+     * <p> An AutonomousMode's name is non-editable.
      *
      * @param name name of the mode (shown to user)
      */
     public AutonomousMode(String name) {
-        this.name = name;
-    }
-
-    /**
-     * Gets the name of the autonomous mode. The name can never change, so in
-     * practice this method could be used to identify whether or not the mode is
-     * the same.
-     *
-     * @return name of the autonomous mode
-     */
-    public String getName() {
-        return name;
+        super(name);
     }
 }
