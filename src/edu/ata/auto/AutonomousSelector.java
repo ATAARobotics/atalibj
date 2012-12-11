3b0c8a5bd6ceaae0dc443984720fac4ffa0c5b7f
joel <joel@joel-laptop>
2012-12-10 22:29:56 -0700

    Fixed documentation and added errors when autonomous mode is not set correctly.

--- src/edu/ata/auto/AutonomousSelector.java
+++ src/edu/ata/auto/AutonomousSelector.java
@@ -6,8 +6,16 @@ import java.util.Hashtable;
 
 /**
  * Singleton design pattern class that stores available autonomous modes and can
- * choose between them by name.
+ * choose between them by name. Acts very similar to a {@link Hashtable},
+ * because it stores data with {@link Hashtable}.
  *
+ * <p> Stores one mode as the 'current' mode. This means that it is the
+ * currently selected mode, otherwise known as the last mode to be passed
+ * through
+ * {@link AutonomousSelector#set(edu.ata.auto.AutonomousMode) set(AutonomousMode)}
+ * or {@link AutonomousSelector#set(java.lang.String) set(String)}.
+ *
+ * @see Hashtable
  * @author Joel Gallant
  */
 public class AutonomousSelector {
@@ -33,20 +41,27 @@ public class AutonomousSelector {
     private final Hashtable modes = new Hashtable();
 
     private AutonomousSelector() {
+        // Singleton instance
     }
 
     /**
-     * Sets the autonomous mode to a previously entered mode, accessing by its
-     * name.
+     * Sets the autonomous mode to a previously entered mode, accessing it by
+     * its name.
      *
+     * @throws AutonomousModeNotSetException thrown when auto mode doesn't exist
      * @param name name of mode
      */
-    public void set(String name) {
+    public void set(String name) throws AutonomousModeNotSetException {
         current = (AutonomousMode) modes.get(name);
+        if (current == null) {
+            throw new AutonomousModeNotSetException("Set the autonomous mode to unavailable mode - " + name);
+        }
     }
 
     /**
-     * Sets the autonomous mode to a new autonomous mode object.
+     * Sets the autonomous mode to a new autonomous mode object. If the object
+     * is already stored, it will not be duplicated. (see
+     * {@link Hashtable#put(java.lang.Object, java.lang.Object)})
      *
      * @param mode mode to set it to
      */
@@ -57,7 +72,8 @@ public class AutonomousSelector {
 
     /**
      * Adds an autonomous mode to the available pool of modes, using its name in
-     * the class.
+     * the class as the accessor in
+     * {@link AutonomousSelector#set(java.lang.String) set(String)}.
      *
      * @param mode mode to add
      */
@@ -76,17 +92,27 @@ public class AutonomousSelector {
 
     /**
      * Gets the currently selected autonomous mode. If one has not been set with
-     * {@code set()}, this method will return {@code null}.
+     * {@code set()}, a {@link AutonomousModeNotSetException} will be thrown.
      *
+     * @throws AutonomousModeNotSetException thrown when autonomous is not set
      * @return currently selected autonomous mode
      */
-    public AutonomousMode get() {
+    public AutonomousMode get() throws AutonomousModeNotSetException {
+        if (current == null) {
+            throw new AutonomousModeNotSetException("The autonomous mode was not set using AutonomousSelector.");
+        }
         return current;
     }
 
     /**
      * Returns a specially formatted string that contains all the available
      * autonomous modes. Used by {@link UserInfo}.
+     *
+     * <p> Ex: Autonomous modes are ShootMiddle, DriveForwards and FeedBalls
+     *
+     * toString() returns:
+     *
+     * <pre> ShootMiddle,DriveForwards,FeedBalls </pre>
      *
      * @return formatted string with all modes, separated by commas
      */
@@ -96,6 +122,23 @@ public class AutonomousSelector {
         while (e.hasMoreElements()) {
             s += e.nextElement() + ",";
         }
-        return s;
+        return s.length() > 0 ? s.substring(0, s.length() - 1) : "Empty";
+    }
+
+    /**
+     * Exception meant to describe to the user that the autonomous mode was not,
+     * or can not be set.
+     */
+    public class AutonomousModeNotSetException extends IllegalStateException {
+
+        /**
+         * Creates exception using a message. Has to include message to clarify
+         * error.
+         *
+         * @param s error message
+         */
+        public AutonomousModeNotSetException(String s) {
+            super(s);
+        }
     }
 }
