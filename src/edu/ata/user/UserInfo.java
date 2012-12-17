a10835bb90948f1b1187a584cbb042a07358795b
Joel Gallant <joelgallant236@gmail.com>
2012-12-16 17:50:01 -0700

    Made transfer rate calculating in a private class TransferRateCalc to keep it modular.

--- src/edu/ata/user/UserInfo.java
+++ src/edu/ata/user/UserInfo.java
@@ -33,8 +33,7 @@ public class UserInfo {
         return instance;
     }
     private final Preferences preferences = Preferences.getInstance();
-    private long lastPPSCheck = System.currentTimeMillis();
-    private int lastPacketsCheck = DriverstationInfo.getPacketCount();
+    private final TransferRateCalc transferRate = new TransferRateCalc();
 
     private UserInfo() {
     }
@@ -51,7 +50,7 @@ public class UserInfo {
      * Receives all info about robot.
      */
     public void updateInfo() {
-        SmartDashboard.putNumber("Packets Per Second", packetsPerSecond());
+        SmartDashboard.putNumber("Packets Per Second", transferRate.packetsPerSecond());
         SmartDashboard.putString("Modules", Module.MODULES.toString());
         SmartDashboard.putString("Robot", Robot.getRobot().getName());
     }
@@ -78,22 +77,6 @@ public class UserInfo {
         }
     }
 
-    private double packetsPerSecond() {
-        return packetsSinceLastCheck() / secondsSinceLastCheck();
-    }
-
-    private long secondsSinceLastCheck() {
-        long s = (lastPPSCheck - System.currentTimeMillis());
-        lastPPSCheck = System.currentTimeMillis();
-        return s / 1000;
-    }
-
-    private int packetsSinceLastCheck() {
-        int s = DriverstationInfo.getPacketCount() - lastPacketsCheck;
-        lastPacketsCheck = DriverstationInfo.getPacketCount();
-        return s;
-    }
-
     private String currentAutonomousMode() {
         if (!preferences.containsKey("currentAuto")) {
             preferences.putString("currentAuto", "DEFAULT");
@@ -113,5 +96,32 @@ public class UserInfo {
             preferences.putString("userScript", "#INSERT SCRIPT HERE");
         }
         return preferences.getString("userScript", null);
+    }
+
+    private class TransferRateCalc {
+
+        private long lastPPSCheck = System.currentTimeMillis();
+        private int lastPacketsCheck = DriverstationInfo.getPacketCount();
+
+        public double packetsPerSecond() {
+            return packetsSinceLastCheck() / secondsSinceLastCheck();
+        }
+        
+        public void reset() {
+            lastPPSCheck = System.currentTimeMillis();
+            lastPacketsCheck = 0;
+        }
+
+        private long secondsSinceLastCheck() {
+            long s = (lastPPSCheck - System.currentTimeMillis());
+            lastPPSCheck = System.currentTimeMillis();
+            return s / 1000;
+        }
+
+        private int packetsSinceLastCheck() {
+            int s = DriverstationInfo.getPacketCount() - lastPacketsCheck;
+            lastPacketsCheck = DriverstationInfo.getPacketCount();
+            return s;
+        }
     }
 }
