6d6187c4bac5e82684e6d75f748d74566ef02b8b
Denis Trailin <hiyou102@gmail.com>
2013-02-19 13:31:45 -0700

    added stop command

--- src/edu/ATA/commands/ArcadeDriveCommand.java
+++ src/edu/ATA/commands/ArcadeDriveCommand.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package edu.ATA.commands;
 
 import edu.ATA.twolf.subsystems.ShiftingDrivetrain;
