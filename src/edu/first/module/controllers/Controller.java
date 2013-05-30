51beac1ee1a211486b6b79e53dcbc7f6e30d48db
Joel Gallant <joelgallant236@gmail.com>
2013-05-30 14:41:10 -0500

    Created Controller module for virtual control

---
+++ src/edu/first/module/controllers/Controller.java
@@ -0,0 +1,111 @@
+package edu.first.module.controllers;
+
+import edu.first.module.Module;
+import edu.first.util.Enum;
+import edu.first.util.MathUtils;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * The general module for virtual controllers. Runs the {@link #run()} method in
+ * a loop whenever the module is enabled.
+ *
+ * <p> The two options for execution are {@link LoopType#FIXED_DELAY} and
+ * {@link LoopType#FIXED_RATE}. A fixed-rate execution tries to ensure your
+ * {@code run()} method is called at the rate given at all times. It will
+ * dynamically adjust to try and run the method at the specified rate. See
+ * {@link Timer#scheduleAtFixedRate(java.util.TimerTask, long, long)} for more
+ * info. A fixed-delay execution waits the specified time after every execution
+ * of {@code run()}. There is no compensation when threads get slow.
+ *
+ * @since May 30 2013
+ * @author Joel Gallant
+ */
+public abstract class Controller extends Module.StartardModule implements Runnable {
+
+    private final int loopTime;
+    private final LoopType.Value loopType;
+    private Timer loopController;
+
+    /**
+     * Constructs the controller with the loop time and the type of loop to run.
+     * Loop types are static instances in {@link LoopType}.
+     *
+     * @param loopTime time in seconds each loop should run
+     * @param loopType kind of execution of the loop
+     */
+    public Controller(double loopTime, Enum.Value loopType) {
+        this.loopTime = (int) MathUtils.round(loopTime * 1000.0);
+        this.loopType = loopType;
+    }
+
+    /**
+     * Constructs the controller with the loop time and the type of loop to run.
+     * Loop types are static instances in {@link LoopType}.
+     *
+     * @param loopTimeHertz the hertz value that represents how fast execution
+     * will happen
+     * @param loopType kind of execution of the loop
+     */
+    public Controller(int loopTimeHertz, Enum.Value loopType) {
+        this(1.0 / (double) loopTimeHertz, loopType);
+    }
+
+    /**
+     * Starts the loop of the controller.
+     */
+    public void enableModule() {
+        if (loopType.equals(LoopType.FIXED_DELAY)) {
+            (loopController = new Timer()).schedule(task(), 0, loopTime);
+        } else if (loopType.equals(LoopType.FIXED_RATE)) {
+            (loopController = new Timer()).scheduleAtFixedRate(task(), 0, loopTime);
+        }
+    }
+
+    /**
+     * Stops the loop of the controller. If it is in the middle of an execution,
+     * that execution will complete before the controller turns off.
+     */
+    public void disableModule() {
+        if (loopController != null) {
+            loopController.cancel();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void init() {
+    }
+
+    private TimerTask task() {
+        return new TimerTask() {
+            public void run() {
+                Controller.this.run();
+            }
+        };
+    }
+
+    /**
+     * Enum representing the different types of loops that a controller can run
+     * in.
+     */
+    public static final class LoopType extends Enum {
+
+        private static final LoopType INSTANCE = new LoopType();
+        /**
+         * A fixed-rate execution tries to ensure your {@code run()} method is
+         * called at the rate given at all times. It will dynamically adjust to
+         * try and run the method at the specified rate. See
+         * {@link Timer#scheduleAtFixedRate(java.util.TimerTask, long, long)}
+         * for more info.
+         */
+        public static final LoopType.Value FIXED_DELAY = INSTANCE.generate("FIXED_DELAY");
+        /**
+         * A fixed-delay execution waits the specified time after every
+         * execution of {@code run()}. There is no compensation when threads get
+         * slow.
+         */
+        public static final LoopType.Value FIXED_RATE = INSTANCE.generate("FIXED_RATE");
+    }
+}
