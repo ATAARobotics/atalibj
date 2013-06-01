628be4fca3209461b7130c9c01783cb494055b99
Joel Gallant <joelgallant236@gmail.com>
2013-06-01 15:03:46 -0600

    Added a Joystick and JoystickModule to get input from the user (drivers).

---
+++ src/edu/first/module/joysticks/Joystick.java
@@ -0,0 +1,75 @@
+package edu.first.module.joysticks;
+
+import edu.first.identifiers.Input;
+import edu.first.identifiers.Position;
+
+/**
+ * General interface that signifies that the class is a joystick that the user
+ * controls functions with. Joysticks have {@link Axis Axises} and
+ * {@link Button Buttons}. Other joystick functions are not part of this
+ * interface, and should be implemented in their respective classes.
+ *
+ * @since June 01 13
+ * @author Joel Gallant
+ */
+public interface Joystick {
+
+    /**
+     * Returns an {@link Axis} that will return the distance that the axis is
+     * at.
+     *
+     * @param port the axis to read
+     * @return the axis object to get values from
+     */
+    public Axis getRawAxis(int port);
+
+    /**
+     * Returns the actual distance that the axis is at.
+     *
+     * @param port the axis to read
+     * @return the value of the axis
+     */
+    public double getRawAxisValue(int port);
+
+    /**
+     * Returns an {@link Button} that will return the position of the button.
+     *
+     * @param port the button to read
+     * @return the button object to get values from
+     */
+    public Button getRawButton(int port);
+
+    /**
+     * Returns the actual position that the button is in.
+     *
+     * @param port the button to read
+     * @return the value of the button
+     */
+    public boolean getRawButtonValue(int port);
+
+    /**
+     * Axis object to get values from an axis on.
+     */
+    public static interface Axis extends Input {
+
+        /**
+         * Returns the actual distance that the axis is at.
+         *
+         * @return the value of the axis
+         */
+        public double get();
+    }
+
+    /**
+     * Button object to get values from a button on.
+     */
+    public static interface Button extends Position {
+
+        /**
+         * Returns the actual position that the button is.
+         *
+         * @return the value of the button
+         */
+        public boolean getPosition();
+    }
+}
---
+++ src/edu/first/module/joysticks/JoystickModule.java
@@ -0,0 +1,248 @@
+package edu.first.module.joysticks;
+
+import edu.first.identifiers.Function;
+import edu.first.module.Module;
+
+/**
+ * A joystick that the user controls functions with. Attaches through the
+ * DriverStation. Has axises and buttons to get input from. Doesn't provide
+ * functionality past raw axises and buttons. ({@link #getRawAxisValue(int)} and
+ * {@link #getRawButtonValue(int)})
+ *
+ * @since June 01 13
+ * @author Joel Gallant
+ */
+public class JoystickModule extends Module.StartardModule implements Joystick {
+
+    private final edu.wpi.first.wpilibj.Joystick joystick;
+    // stores axises for quick access and not having to instantize everytime
+    private Axis[] axises = new Axis[]{
+        new RawAxis(1),
+        new RawAxis(2),
+        new RawAxis(3),
+        new RawAxis(4),
+        new RawAxis(5),
+        new RawAxis(6)
+    };
+    // stores buttons for quick access and not having to instantize everytime
+    private Button[] buttons = new Button[]{
+        new RawButton(1),
+        new RawButton(2),
+        new RawButton(3),
+        new RawButton(4),
+        new RawButton(5),
+        new RawButton(6),
+        new RawButton(7),
+        new RawButton(8),
+        new RawButton(9),
+        new RawButton(10),
+        new RawButton(11),
+        new RawButton(12)
+    };
+
+    /**
+     * Constructs the joystick with the {@link edu.wpi.first.wpilibj.Joystick}
+     * object that this joystick gets input from.
+     *
+     * @param joystick the composing instance to get input from
+     */
+    public JoystickModule(edu.wpi.first.wpilibj.Joystick joystick) {
+        this.joystick = joystick;
+    }
+
+    /**
+     * Constructs the joystick with a port on the DriverStation.
+     *
+     * @param port channel in configuration of DriverStation
+     */
+    public JoystickModule(int port) {
+        this(new edu.wpi.first.wpilibj.Joystick(port));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void enableModule() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void disableModule() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void init() {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws IndexOutOfBoundsException when port is not available
+     */
+    public final Axis getRawAxis(int port) {
+        checkAxis(port);
+        return axises[port - 1];
+
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws IllegalStateException when module is not enabled
+     */
+    public final double getRawAxisValue(int port) {
+        return getRawAxis(port).get();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws IndexOutOfBoundsException when port is not available
+     */
+    public final Button getRawButton(int port) {
+        checkButton(port);
+        return buttons[port - 1];
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws IllegalStateException when module is not enabled
+     */
+    public final boolean getRawButtonValue(int port) {
+        return getRawButton(port).getPosition();
+    }
+
+    /**
+     * Applies the function to {@link #getRawAxisValue(int)} and
+     * {@link #getRawAxis(int)}. All input will have this function (and only
+     * this function) applied.
+     *
+     * @throws IndexOutOfBoundsException when port is not available
+     * @param port the axis to read
+     * @param function change to apply to input
+     */
+    public final void changeAxis(int port, final Function function) {
+        checkAxis(port);
+        final Axis old = axises[port - 1];
+        axises[port - 1] = new Axis() {
+            public double get() {
+                return function.F(old.get());
+            }
+        };
+    }
+
+    /**
+     * Changes the axis to a different input when called from
+     * {@link #getRawAxisValue(int)} and {@link #getRawAxis(int)}. All input on
+     * that port will be from the axis given.
+     *
+     * @throws IndexOutOfBoundsException when port is not available
+     * @param port the axis to change
+     * @param axis new axis
+     */
+    public final void setAxis(int port, final Axis axis) {
+        checkAxis(port);
+        axises[port - 1] = axis;
+    }
+
+    /**
+     * Changes the button to a different input when called from
+     * {@link #getRawButtonValue(int)} and {@link #getButtonAxis(int)}. All
+     * input on that port will be from the button given.
+     *
+     * @param port the button to change
+     * @param button new button
+     */
+    public final void setButton(int port, final Button button) {
+        checkButton(port);
+        buttons[port - 1] = button;
+    }
+
+    /**
+     * Inverts input from the button when called from
+     * {@link #getRawButtonValue(int)} and {@link #getRawButton(int)}.
+     *
+     * @param port the button to invert
+     */
+    public final void invertButton(int port) {
+        checkButton(port);
+        final Button old = buttons[port - 1];
+        buttons[port - 1] = new Button() {
+            public boolean getPosition() {
+                return !old.getPosition();
+            }
+        };
+    }
+
+    /**
+     * Increases the capacity of the axises on this joystick. The new axises
+     * will not function at all until
+     * {@link #setAxis(int, edu.first.module.joysticks.Joystick.Axis)} has been
+     * called.
+     *
+     * @param newAxises how many axises to create
+     */
+    protected final void increaseAxisCapacity(int newAxises) {
+        Axis[] b = new Axis[axises.length + newAxises];
+        System.arraycopy(axises, 0, b, 0, axises.length);
+        axises = b;
+    }
+
+    /**
+     * Increases the capacity of the buttons on this joystick. The new buttons
+     * will not function at all until
+     * {@link #setButton(int, edu.first.module.joysticks.Joystick.Button)} has
+     * been called.
+     *
+     * @param newButtons how many buttons to create
+     */
+    protected final void increaseButtonCapacity(int newButtons) {
+        Button[] b = new Button[buttons.length + newButtons];
+        System.arraycopy(buttons, 0, b, 0, buttons.length);
+        buttons = b;
+    }
+
+    private void checkAxis(int port) {
+        if (port > axises.length || port < 1) {
+            throw new IndexOutOfBoundsException("Axis " + port + " is not a valid axis");
+        }
+    }
+
+    private void checkButton(int port) {
+        if (port > buttons.length || port < 1) {
+            throw new IndexOutOfBoundsException("Button " + port + " is not a valid button");
+        }
+    }
+
+    private class RawAxis implements Axis {
+
+        private final int port;
+
+        private RawAxis(int port) {
+            this.port = port;
+        }
+
+        public double get() {
+            ensureEnabled();
+            return joystick.getRawAxis(port);
+        }
+    }
+
+    private class RawButton implements Button {
+
+        private final int port;
+
+        private RawButton(int port) {
+            this.port = port;
+        }
+
+        public boolean getPosition() {
+            ensureEnabled();
+            return joystick.getRawButton(port);
+        }
+    }
+}
