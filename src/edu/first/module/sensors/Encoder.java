24894d2a01b624bf76b1d19bc5d7d74aac45dc5a
Joel Gallant <joelgallant236@gmail.com>
2013-06-03 20:11:07 -0600

    Added encoder to close #12

---
+++ src/edu/first/module/sensors/Encoder.java
@@ -0,0 +1,48 @@
+package edu.first.module.sensors;
+
+import edu.first.identifiers.PositionalSensor;
+import edu.first.identifiers.RateSensor;
+
+/**
+ * General interface to represent encoders. Encoders should be able to measure
+ * distance and rate.
+ *
+ * @since June 03 13
+ * @author Joel Gallant
+ */
+public interface Encoder extends PositionalSensor, RateSensor {
+
+    /**
+     * Resets the distance of the encoder.
+     */
+    public void reset();
+
+    /**
+     * Returns an un-scaled distance value.
+     *
+     * @return unchanged distance value
+     */
+    public int getRaw();
+
+    /**
+     * Returns the distance that the encoder has measured.
+     *
+     * @return distance measured
+     */
+    public double getPosition();
+
+    /**
+     * Returns how fast encoder pulses are happening.
+     *
+     * @return current speed of pulses
+     */
+    public double getRate();
+
+    /**
+     * Returns a value indicative of the status of the encoder. Should be
+     * implementation-specific.
+     *
+     * @return value of encoder
+     */
+    public double get();
+}
