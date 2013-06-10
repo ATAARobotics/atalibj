8d6b014effac0d4fcdfb515c2883137d0470e8b6
joelg236 <joelgallant236@gmail.com>
2013-06-09 21:44:41 -0600

    Made performance adjustments to ArrayList that (on my machine) improve performance by > 2500%.

--- src/edu/first/util/list/ArrayList.java
+++ src/edu/first/util/list/ArrayList.java
@@ -1,27 +1,40 @@
 package edu.first.util.list;
 
-import java.util.NoSuchElementException;
-
 /**
  * A non-type-safe non-thread-safe re-sizable array that automatically adjusts
  * to growth and decline of elements. Holds references to a list of objects and
  * gives outside access to them.
  *
- * This implementation is built to give a more robust and consistent list than
- * {@link edu.wpi.first.wpilibj.networktables2.util.List}. It provides all
+ * <p> This implementation is built to give a more robust and consistent list
+ * than {@link edu.wpi.first.wpilibj.networktables2.util.List}. It provides all
  * needed methods to add, remove and get elements from the array.
+ *
+ * <p> <i>*A small caveat - this implementation performs at 1-5% slower than the
+ * official ArrayList implementation on JDK7. The performance differences are so
+ * minimal that you should not worry about them.</i>
  *
  * @since May 15 13
  * @author Joel Gallant
  */
 public final class ArrayList implements List {
 
-    private final ResizingArray array = new ResizingArray();
+    private transient Object[] elementData;
+    private int size = 0;
+
+    /**
+     * Constructs an empty array list with no elements.
+     *
+     * @param capacity expected amount of elements to stage performance for
+     */
+    public ArrayList(int capacity) {
+        this.elementData = new Object[capacity];
+    }
 
     /**
      * Constructs an empty array list with no elements.
      */
     public ArrayList() {
+        this(10);
     }
 
     /**
@@ -30,21 +43,161 @@ public final class ArrayList implements List {
      * @param collection object with a group of elements to add off the start
      */
     public ArrayList(Collection collection) {
-        addAll(collection);
+        this.elementData = collection.toArray();
+        if (elementData.getClass() != Object[].class) {
+            elementData = copyOf(elementData, size);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws IndexOutOfBoundsException when {@code index < 0 && index > size}
+     */
+    public void addAll(int index, Collection c) {
+        checkIndexAdd(index);
+
+        Object[] a = c.toArray();
+        int numNew = a.length;
+        ensureCapacity(size + numNew);
+
+        int numMoved = size - index;
+        if (numMoved > 0) {
+            System.arraycopy(elementData, index, elementData, index + numNew,
+                    numMoved);
+        }
+
+        System.arraycopy(a, 0, elementData, index, numNew);
+        size += numNew;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws IndexOutOfBoundsException when {@code index < 0 && index >= size}
+     */
+    public Object get(int index) {
+        checkIndex(index);
+        return elementData[index];
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws IndexOutOfBoundsException when {@code index < 0 && index >= size}
+     */
+    public Object set(int index, Object element) {
+        checkIndex(index);
+        Object old = elementData[index];
+        elementData[index] = element;
+        return old;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws IndexOutOfBoundsException when {@code index < 0 && index > size}
+     */
+    public void add(int index, Object element) {
+        checkIndexAdd(index);
+        ensureCapacity(size + 1);
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = element;
+        size++;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws IndexOutOfBoundsException when {@code index < 0 && index >= size}
+     */
+    public Object remove(int index) {
+        checkIndex(index);
+
+        Object oldValue = elementData[index];
+
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(elementData, index + 1, elementData, index,
+                    numMoved);
+        }
+        elementData[--size] = null;
+
+        return oldValue;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int indexOf(Object o) {
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elementData[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(elementData[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int lastIndexOf(Object o) {
+        if (o == null) {
+            for (int i = size - 1; i >= 0; i--) {
+                if (elementData[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = size - 1; i >= 0; i--) {
+                if (o.equals(elementData[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws IndexOutOfBoundsException when {@code index < 0 && index >= size}
+     */
+    public List subList(int fromIndex, int toIndex) {
+        checkIndex(fromIndex);
+        checkIndex(toIndex);
+
+        if (fromIndex > toIndex) {
+            throw new IndexOutOfBoundsException(fromIndex + "  is higher than " + toIndex);
+        }
+
+        List l = new ArrayList((toIndex - fromIndex) * 2);
+        for (int x = fromIndex; x <= toIndex; x++) {
+            l.add(elementData[x]);
+        }
+        return l;
     }
 
     /**
      * {@inheritDoc}
      */
     public int size() {
-        return array.size();
+        return size;
     }
 
     /**
      * {@inheritDoc}
      */
     public boolean isEmpty() {
-        return array.size() == 0;
+        return size == 0;
     }
 
     /**
@@ -54,8 +207,8 @@ public final class ArrayList implements List {
      * @param o element whose presence in this collection is to be tested
      * @return if this collection contains the specified element
      */
-    public boolean contains(Object object) {
-        return indexOf(object) != -1;
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
     }
 
     /**
@@ -64,25 +217,28 @@ public final class ArrayList implements List {
      * <p> The returned array will be in the order of this list.
      */
     public Object[] toArray() {
-        Object[] a = new Object[size()];
-        for (int x = 0; x < a.length; x++) {
-            a[x] = get(x);
+        // array is guaranteed to be the size
+        Object[] r = new Object[size];
+        Iterator it = iterator();
+        for (int i = 0; i < r.length; i++) {
+            if (!it.hasNext()) // fewer elements than expected
+            {
+                return copyOf(r, i);
+            }
+            r[i] = it.next();
         }
-        return a;
+        return r;
     }
 
     /**
      * Inserts the element into the collection. This method will always put the
      * element at the end of the collection.
      *
-     * This list does not allow {@code null} elements. A
-     * {@link NullPointerException} will be thrown if a null value is added.
-     *
      * @param e element to add to the collection
-     * @throws NullPointerException when {@code o} is added
      */
-    public void add(Object o) {
-        array.add(o);
+    public void add(Object e) {
+        ensureCapacity(size + 1);
+        elementData[size++] = e;
     }
 
     /**
@@ -91,8 +247,22 @@ public final class ArrayList implements List {
      *
      * @param o element to remove from the collection
      */
-    public void remove(Object object) {
-        array.remove(object);
+    public void remove(Object o) {
+        if (o == null) {
+            for (int index = 0; index < size; index++) {
+                if (elementData[index] == null) {
+                    fastRemove(index);
+                    return;
+                }
+            }
+        } else {
+            for (int index = 0; index < size; index++) {
+                if (o.equals(elementData[index])) {
+                    fastRemove(index);
+                    return;
+                }
+            }
+        }
     }
 
     /**
@@ -112,10 +282,11 @@ public final class ArrayList implements List {
      * {@inheritDoc}
      */
     public void addAll(Collection c) {
-        Iterator i = c.iterator();
-        while (i.hasNext()) {
-            add(i.next());
-        }
+        Object[] a = c.toArray();
+        int numNew = a.length;
+        ensureCapacity(size + numNew);
+        System.arraycopy(a, 0, elementData, size, numNew);
+        size += numNew;
     }
 
     /**
@@ -124,7 +295,10 @@ public final class ArrayList implements List {
     public void removeAll(Collection c) {
         Iterator i = c.iterator();
         while (i.hasNext()) {
-            remove(i.next());
+            Object o = i.next();
+            if (contains(o)) {
+                remove(o);
+            }
         }
     }
 
@@ -134,9 +308,9 @@ public final class ArrayList implements List {
     public void retainAll(Collection c) {
         Iterator i = iterator();
         while (i.hasNext()) {
-            Object next = i.next();
-            if (!c.contains(next)) {
-                remove(next);
+            Object o = i.next();
+            if (!c.contains(o)) {
+                remove(o);
             }
         }
     }
@@ -145,91 +319,23 @@ public final class ArrayList implements List {
      * {@inheritDoc}
      */
     public void clear() {
-        array.clear();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public void addAll(int index, Collection c) {
-        Iterator i = c.iterator();
-        for (; i.hasNext(); index++) {
-            add(index, i.next());
+        Iterator i = iterator();
+        while (i.hasNext()) {
+            remove(i.next());
         }
     }
 
     /**
-     * {@inheritDoc}
+     * Ensures that the array list is configured to hold the amount of objects
+     * given. This method is provided to improve performance. If you plan to add
+     * a lot of elements, this method should be used.
      *
-     * @throws IndexOutOfBoundsException when {@code index < 0 && index >= size}
+     * @param min minimum capacity
      */
-    public Object get(int index) {
-        return array.get(index);
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @throws NullPointerException when {@code element} is {@code null}
-     * @throws IndexOutOfBoundsException when {@code index < 0 && index >= size}
-     */
-    public Object set(int index, Object element) {
-        return array.set(index, element);
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @throws NullPointerException when {@code element} is {@code null}
-     */
-    public void add(int index, Object element) {
-        array.add(index, element);
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @throws IndexOutOfBoundsException when {@code index < 0 && index >= size}
-     */
-    public Object remove(int index) {
-        Object o = get(index);
-        remove(o);
-        return o;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public int indexOf(Object o) {
-        for (int x = 0; x < size(); x++) {
-            if (o.equals(get(x))) {
-                return x;
-            }
+    public void ensureCapacity(int min) {
+        if (min > elementData.length) {
+            growTo(min);
         }
-        return -1;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public int lastIndexOf(Object o) {
-        for (int x = size() - 1; x >= 0; x--) {
-            if (o.equals(get(x))) {
-                return x;
-            }
-        }
-        return -1;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public List subList(int fromIndex, int toIndex) {
-        ArrayList l = new ArrayList();
-        for (int x = fromIndex; x <= toIndex; x++) {
-            l.add(get(x));
-        }
-        return l;
     }
 
     /**
@@ -238,113 +344,59 @@ public final class ArrayList implements List {
      * Elements are in the order that they are in this list.
      */
     public Iterator iterator() {
-        return new Itr(toArray());
+        return new Iterator() {
+            final Object[] e = copyOf(elementData, size);
+            int index = 0;
+
+            public boolean hasNext() {
+                return index < e.length;
+            }
+
+            public Object next() {
+                return e[index++];
+            }
+        };
     }
 
-    // Private iterator implementation
-    private static class Itr implements Iterator {
-
-        private final Object[] elements;
-        private int p = 0;
-
-        private Itr(Object[] elements) {
-            this.elements = elements;
+    /**
+     * Compares the specified object with this list for equality. Returns
+     * {@code true} if and only if the specified object is also a list, both
+     * lists have the same size, and all corresponding pairs of elements in the
+     * two lists are <i>equal</i>. (Two elements {@code e1} and {@code e2} are
+     * <i>equal</i> if {@code (e1==null ? e2==null :
+     * e1.equals(e2))}.) In other words, two lists are defined to be equal if
+     * they contain the same elements in the same order.<p>
+     *
+     * This implementation first checks if the specified object is this list. If
+     * so, it returns {@code true}; if not, it checks if the specified object is
+     * a list. If not, it returns {@code false}; if so, it iterates over both
+     * lists, comparing corresponding pairs of elements. If any comparison
+     * returns {@code false}, this method returns {@code false}. If either
+     * iterator runs out of elements before the other it returns {@code false}
+     * (as the lists are of unequal length); otherwise it returns {@code true}
+     * when the iterations complete.
+     *
+     * @param o the object to be compared for equality with this list
+     * @return {@code true} if the specified object is equal to this list
+     */
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof List)) {
+            return false;
         }
 
-        public boolean hasNext() {
-            return p < elements.length;
-        }
-
-        public Object next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            return elements[p++];
-        }
-    }
-
-    private static final class ResizingArray {
-
-        private Object[] elements = new Object[0];
-
-        private void add(Object o) {
-            if (o == null) {
-                throw new NullPointerException();
-            }
-            Object[] b = new Object[elements.length + 1];
-            System.arraycopy(elements, 0, b, 0, elements.length);
-            elements = b;
-
-            elements[elements.length - 1] = o;
-        }
-
-        private void add(int i, Object o) {
-            if (i >= size()) {
-                throw new IndexOutOfBoundsException("Index " + i + " is out of bounds.");
-            }
-            if (o == null) {
-                throw new NullPointerException();
-            }
-
-            Object[] b = new Object[elements.length + 1];
-            System.arraycopy(elements, 0, b, 0, i);
-            System.arraycopy(elements, i, b, i + 1, elements.length - i);
-            elements = b;
-            
-            elements[i] = o;
-        }
-
-        private Object set(int x, Object o) {
-            if (o == null) {
-                throw new NullPointerException();
-            }
-            if (x >= size()) {
-                throw new IndexOutOfBoundsException("Index " + x + " is out of bounds.");
-            }
-            Object p = elements[x];
-            elements[x] = o;
-            return p;
-        }
-
-        private void remove(Object o) {
-            for (int x = 0; x < elements.length; x++) {
-                if (o.equals(elements[x])) {
-                    elements[x] = null;
-                    shrink(x);
-                    return;
-                }
+        Iterator e1 = iterator();
+        Iterator e2 = ((List) o).iterator();
+        while (e1.hasNext() && e2.hasNext()) {
+            Object o1 = e1.next();
+            Object o2 = e2.next();
+            if (!(o1 == null ? o2 == null : o1.equals(o2))) {
+                return false;
             }
         }
-
-        private Object get(int i) {
-            if (i >= size()) {
-                throw new IndexOutOfBoundsException("Index " + i + " is out of bounds.");
-            }
-            return elements[i];
-        }
-
-        private void clear() {
-            elements = new Object[0];
-        }
-
-        private int size() {
-            return elements.length;
-        }
-
-        private void shrink(int x) {
-            for (; x < elements.length; x++) {
-                if (elements[x] == null) {
-
-                    Object[] b = new Object[elements.length - 1];
-                    System.arraycopy(elements, 0, b, 0, x);
-                    System.arraycopy(elements, x + 1, b, x, elements.length - x - 1);
-                    elements = b;
-                    
-                    shrink(x + 1);
-                    return;
-                }
-            }
-        }
+        return !(e1.hasNext() || e2.hasNext());
     }
 
     /**
@@ -373,5 +425,36 @@ public final class ArrayList implements List {
             }
             sb.append(',').append(' ');
         }
+    }
+
+    private void checkIndex(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds (0 - " + (size - 1) + ")");
+        }
+    }
+
+    private void checkIndexAdd(int index) {
+        if (index > size || index < 0) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds (0 - " + (size - 1) + ")");
+        }
+    }
+
+    private void growTo(int min) {
+        elementData = copyOf(elementData, min);
+    }
+
+    private void fastRemove(int index) {
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(elementData, index + 1, elementData, index,
+                    numMoved);
+        }
+        elementData[--size] = null; // Let gc do its work
+    }
+
+    private Object[] copyOf(Object[] o, int size) {
+        Object[] c = new Object[size];
+        System.arraycopy(o, 0, c, 0, Math.min(o.length, size));
+        return c;
     }
 }
