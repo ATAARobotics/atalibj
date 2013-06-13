04a6486876d854c8cd563779627ab00fb7081c73
joelg236 <joelgallant236@gmail.com>
2013-06-13 01:20:40 -0600

    Added some abstractions for static input and transforming output. Makes binds easier.

---
+++ src/edu/first/identifiers/StaticInput.java
@@ -0,0 +1,30 @@
+package edu.first.identifiers;
+
+/**
+ * Never changing input that will always return the same thing.
+ *
+ * @since June 13 13
+ * @author Joel Gallant
+ */
+public final class StaticInput implements Input {
+
+    private final double input;
+
+    /**
+     * Constructs the input using the value to use.
+     *
+     * @param input value that will always be returned
+     */
+    public StaticInput(double input) {
+        this.input = input;
+    }
+
+    /**
+     * Returns the input given in the constructor.
+     *
+     * @return value that will always be used
+     */
+    public double get() {
+        return input;
+    }
+}
---
+++ src/edu/first/identifiers/TransformedOutput.java
@@ -0,0 +1,34 @@
+package edu.first.identifiers;
+
+/**
+ * An output that abstracts a function on top of it to change input before
+ * getting to the output.
+ *
+ * @since June 13 13
+ * @author Joel Gallant
+ */
+public final class TransformedOutput implements Output {
+
+    private final Output output;
+    private final Function function;
+
+    /**
+     * Constructs the output using the function to apply.
+     *
+     * @param output underlying output to send input to
+     * @param function transforming function that changes input
+     */
+    public TransformedOutput(Output output, Function function) {
+        this.output = output;
+        this.function = function;
+    }
+
+    /**
+     * Returns the transformed input.
+     *
+     * @param value original value changed by function
+     */
+    public void set(double value) {
+        output.set(function.F(value));
+    }
+}
