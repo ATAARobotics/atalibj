7079193bdd8ece27a3eff2dddbb65a622688cb23
Joel Gallant <joelgallant236@gmail.com>
2013-06-22 20:23:21 -0600

    Fixed small type in documentation.

--- src/edu/first/lang/OutOfSyncException.java
+++ src/edu/first/lang/OutOfSyncException.java
@@ -18,7 +18,7 @@ public class OutOfSyncException extends IllegalStateException {
     }
 
     /**
-     * Constructs an IllegalStateException with the specified detail message. A
+     * Constructs an OutOfSyncException with the specified detail message. A
      * detail message is a String that describes this particular exception.
      *
      * @param s the String that contains a detailed message
