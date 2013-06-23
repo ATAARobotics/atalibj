5a2a5a7ff190e95d4cf79001016f6fefb228cf8b
joelg236 <joelgallant236@gmail.com>
2013-06-23 14:37:43 -0600

    Added SmartDashboard interactive classes.

---
+++ src/edu/first/util/dashboard/StringDashboard.java
@@ -0,0 +1,70 @@
+package edu.first.util.dashboard;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.tables.TableKeyNotDefinedException;
+
+/**
+ * General class that interacts with string values on the
+ * {@link SmartDashboard}.
+ *
+ * @since June 23 13
+ * @author Joel Gallant
+ */
+public class StringDashboard {
+
+    private final String key;
+    private final String defaultValue;
+
+    /**
+     * Constructs the dashboard value with the key used to access it.
+     *
+     * @param key unique key that accesses the value
+     * @param defaultValue value to return if none exists
+     */
+    public StringDashboard(String key, String defaultValue) {
+        this.key = key;
+        this.defaultValue = defaultValue;
+    }
+
+    /**
+     * Returns the key of the dashboard value that is used to access it.
+     *
+     * @return key in smart dashboard
+     */
+    public String getKey() {
+        return key;
+    }
+    
+    /**
+     * Returns whether or not the value is currently stored on the CRIO.
+     *
+     * @return if value exists
+     */
+    public boolean exists() {
+        try {
+            SmartDashboard.getString(key);
+            return true;
+        } catch (TableKeyNotDefinedException ex) {
+            return false;
+        }
+    }
+
+    /**
+     * Returns the current value associated with the key. Will return the
+     * default value when it does not {@link #exists() exist}.
+     *
+     * @return current value
+     */
+    public String get() {
+        return SmartDashboard.getString(key, defaultValue);
+    }
+    
+    /**
+     * Sets the value to be associated with the key.
+     *
+     * @param val new value to set
+     */
+    public void set(String val) {
+        SmartDashboard.putString(key, val);
+    }
+}
