0bc48c66eba905e04b8f3fe340af50b4db789668
Joel David Gallant <joelgallant236@gmail.com>
2014-03-10 20:28:52 -0600

    Added some useful common commands

---
+++ src/edu/first/commands/common/AddButtonBind.java
@@ -0,0 +1,23 @@
+package edu.first.commands.common;
+
+import edu.first.command.Command;
+import edu.first.module.joysticks.BindingJoystick;
+
+/**
+ *
+ * @author Joel Gallant <joelgallant236@gmail.com>
+ */
+public class AddButtonBind implements Command {
+
+    private final BindingJoystick joystick;
+    private final BindingJoystick.ButtonBind bind;
+
+    public AddButtonBind(BindingJoystick joystick, BindingJoystick.ButtonBind bind) {
+        this.joystick = joystick;
+        this.bind = bind;
+    }
+
+    public void run() {
+        joystick.addButtonBind(bind);
+    }
+}
---
+++ src/edu/first/commands/common/DisableModule.java
@@ -0,0 +1,27 @@
+package edu.first.commands.common;
+
+import edu.first.command.Command;
+import edu.first.module.Module;
+
+/**
+ *
+ * @author Joel Gallant <joelgallant236@gmail.com>
+ */
+public class DisableModule implements Command {
+
+    private final Module[] modules;
+
+    public DisableModule(Module[] modules) {
+        this.modules = modules;
+    }
+
+    public DisableModule(Module module) {
+        this.modules = new Module[]{module};
+    }
+
+    public void run() {
+        for (int x = 0; x < modules.length; x++) {
+            modules[x].disable();
+        }
+    }
+}
