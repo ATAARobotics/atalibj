c23ccbaf799f9f53244fa786ba85dc579b23453b
Joel David Gallant <joelgallant236@gmail.com>
2014-03-28 17:50:39 -0600

    Added a static string input

---
+++ src/edu/first/identifiers/StaticStringInput.java
@@ -0,0 +1,24 @@
+package edu.first.identifiers;
+
+/**
+ * String input that will always return the same value.
+ *
+ * @author Joel Gallant <joelgallant236@gmail.com>
+ */
+public class StaticStringInput implements StringInput {
+
+    private final String input;
+
+    /**
+     * Constructs the input with the value to always return.
+     *
+     * @param input value to return
+     */
+    public StaticStringInput(String input) {
+        this.input = input;
+    }
+
+    public String getValue() {
+        return input;
+    }
+}
